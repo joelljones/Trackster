161305876dc4b6f766b445b77a0884e8a5395e42
Joell Jones <joelljones86@gmail.com>
2018-03-19 16:21:43 -0400

    display the song list

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -13,8 +13,31 @@ $(function() {
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
+// method takes a single parameter, an array of track data (the array that is currently being logged in Trackster.searchTracksByTitle())
 Trackster.renderTracks = function(tracks) {
+  var $trackList = $('#track-list');
 
+  $trackList.empty();
+
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var mediumAlbumArt = track.image[1]["#text"];
+    var htmlTrackRow =
+      '<div class="row track">' +
+      '  <div class="col-xs-1 col-xs-offset-1 play-button">' +
+      '    <a href="'+ track.url + '" target="_blank">' +
+      '      <i class="fa fa-play-circle-o fa-2x"></i>' +
+      '    </a>' +
+      '  </div>' +
+      '  <div class="col-xs-4">' + track.name + '</div>' +
+      '  <div class="col-xs-2">' + track.artist + '</div>' +
+      '  <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>' +
+      '  <div class="col-xs-2">' + track.listeners + '</div>' +
+      '</div>';
+
+    // add this new track HTML to the list of tracks
+    $trackList.append(htmlTrackRow);
+  }
 };
 
 /*
@@ -25,7 +48,7 @@ Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
     success: function(response) {
-      console.log(response);
+      Trackster.renderTracks(response.results.trackmatches.track);
     }
   });
 };
